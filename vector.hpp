02ffc716bf74cbec60af6a4636cd2bdd4aaeccc5
Theo DESMET <tdesmet@e1r3p18.clusters.42paris.fr>
2023-01-11 09:17:38 +0100

    add ~vector, vector[], vector::push_back()

--- vector.hpp
+++ vector.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <memory>
 
 namespace ft{
@@ -11,8 +13,8 @@ namespace ft{
 
 			typedef T											value_type;
 			typedef Allocator									allocator_type;
-			typedef typename Allocator_type::reference			reference;
-			typedef typename Allocator_type::const_reference	const_reference;
+			typedef typename allocator_type::reference			reference;
+			typedef typename allocator_type::const_reference	const_reference;
 			typedef typename allocator_type::pointer			pointer;
 			typedef typename allocator_type::const_pointer		const_pointer;
 			typedef typename std::ptrdiff_t 					difference_type;
@@ -23,12 +25,12 @@ namespace ft{
 			*/
 
 			explicit vector (const allocator_type& alloc = allocator_type())
-			: _ref_alloc(alloc), _ptr_t(NULL), _allocaded_mem(0)
+			: _alloc(alloc), _ptr_t(NULL), _allocaded_mem(0)
 			, _used_mem(0) {};
 
 			explicit vector (size_type n, const value_type& val = value_type(),
 				const allocator_type& alloc = allocator_type())
-			: _ref_alloc(alloc), _ptr_t(&val), _allocaded_mem(n)
+			: _alloc(alloc), _ptr_t(&val), _allocaded_mem(n)
 			, _used_mem(n) {
 				//new n
 			};
@@ -36,7 +38,7 @@ namespace ft{
 			template <class InputIterator>
 			vector (InputIterator first, InputIterator last, 
 				const allocator_type& alloc = allocator_type())
-			: _ref_alloc(alloc), _ptr_t(NULL) {
+			: _alloc(alloc), _ptr_t(NULL) {
 				//push_back
 			};
 
@@ -44,16 +46,43 @@ namespace ft{
 				//copy
 			};
 
-			virtual ~destructor() {
-				//delete all
+			virtual ~vector() {
+				for (std::size_t i = 0; i < _used_mem; i++) {
+					_alloc.destroy(_ptr_t + i);
+				}
+				if (_allocaded_mem != 0) 
+					_alloc.deallocate(_ptr_t, _allocaded_mem);
 			};
+
+			/*
+			**	operator overload
+			*/
+
+			reference operator[] (size_type n) {
+				return (this->_ptr_t[n]);
+			};
+
+			const_reference operator[] (size_type n) const {
+				return (this->_ptr_t[n]);
+			};
+
+			/*	
+			**	modifiers fonction:
+			*/
+
+			void push_back (const value_type& val){
+				if (_used_mem == _allocaded_mem)
+					_extend_mem_();
+				_alloc.construct(_ptr_t + _used_mem, val);
+				_used_mem++;
+			}
 
 
 		/*	
 		**	private members
 		*/
 
-		Allocator	&_ref_alloc;
+		Allocator	_alloc;
 		T			*_ptr_t;
 		std::size_t	_allocaded_mem;
 		std::size_t	_used_mem;
@@ -61,22 +90,21 @@ namespace ft{
 		//double the mem size
 		void	_extend_mem_() {
 			if (!_allocaded_mem) {
-				_ptr_t = _ref_alloc.allocate(1);
+				_ptr_t = _alloc.allocate(1);
 				_allocaded_mem++;
 			} else {
-				T	*new_ptr_t = _ref_alloc.allocate(
+				T	*new_ptr_t = _alloc.allocate(
 					_allocaded_mem * 2, _ptr_t);
-				_allocaded_mem *= 2;
 				if (new_ptr_t != _ptr_t) {
-					for (std::size_t i = 0, i < _used_mem, i++) {
-						_ref_alloc.construct(new_ptr_t + i, *(_ptr_t + i))
-						_ref_alloc.destroy(_ptr_t + i);
+					for (std::size_t i = 0; i < _used_mem; i++) {
+						_alloc.construct(new_ptr_t + i, *(_ptr_t + i));
+						_alloc.destroy(_ptr_t + i);
 					}
-					_ref_alloc.deallocate(_ptr_t);
+					_alloc.deallocate(_ptr_t, _allocaded_mem);
 					_ptr_t = new_ptr_t;
 				}
+				_allocaded_mem *= 2;
 			}
 		}
-
 	};
 }
